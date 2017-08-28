14e876a590e683b45ea67c7f716ab0b7bed87938
mfrachet <marvin.frachet@gmail.com>
2017-08-28 08:02:11 +0200

    Adding Result, Search and Card component
    Adding Result, Search and Card component

--- exercices/exercice-2/src/components/Result.vue
+++ exercices/exercice-2/src/components/Result.vue
@@ -2,7 +2,7 @@
   <div class="resultset">
     <div class="columns is-multiline">
       <div class="column is-2 has-text-centered">
-        <Card name="Bulbuzar" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
+        <Card name="Bulbasaur" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
       </div>
     </div>
   </div>
