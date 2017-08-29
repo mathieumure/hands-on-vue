cd4f76eb7613b5a31953a75ab79ab6cdf2314c40
mfrachet <marvin.frachet@gmail.com>
2017-08-29 19:01:05 +0200

    init ex3

---
+++ exercices/exercice-3/src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="resultset">
+    <div class="columns is-multiline">
+      <div class="column is-2 has-text-centered">
+        <Card :name="pokemons[0].name" :image="pokemons[0].image"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+export default {
+  name: 'result',
+  components : {
+    Card,
+  },
+  data() {
+    return {
+      pokemons: [{
+        name: 'Bulbasaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      }],
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
