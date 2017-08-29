4e6708555381953df57f0d5e13105a387142c275
mfrachet <marvin.frachet@gmail.com>
2017-08-29 18:52:41 +0200

    Using a list instead of hard chars

--- exercices/exercice-2/src/components/Result.vue
+++ exercices/exercice-2/src/components/Result.vue
@@ -2,7 +2,7 @@
   <div class="resultset">
     <div class="columns is-multiline">
       <div class="column is-2 has-text-centered">
-        <Card name="Bulbasaur" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"/>
+        <Card :name="pokemons[0].name" :image="pokemons[0].image"/>
       </div>
     </div>
   </div>
@@ -14,6 +14,14 @@ export default {
   name: 'result',
   components : {
     Card,
+  },
+  data() {
+    return {
+      pokemons: [{
+        name: 'Bulbasaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      }],
+    };
   },
 }
 </script>
