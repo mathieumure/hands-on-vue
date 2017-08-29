5bf2aa602c722e6e6e51cec8f6b56ddf46edfe41
mfrachet <marvin.frachet@gmail.com>
2017-08-29 19:05:53 +0200

    Add v-for

--- exercices/exercice-3/src/components/Result.vue
+++ exercices/exercice-3/src/components/Result.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="resultset">
     <div class="columns is-multiline">
-      <div class="column is-2 has-text-centered">
-        <Card :name="pokemons[0].name" :image="pokemons[0].image"/>
+      <div class="column is-2 has-text-centered" v-for="pokemon in pokemons">
+        <Card :name="pokemon.name" :image="pokemon.image"/>
       </div>
     </div>
   </div>
@@ -19,6 +19,33 @@ export default {
     return {
       pokemons: [{
         name: 'Bulbasaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      }, {
+        name: 'Ivysaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+      }, {
+        name: 'Venusaur',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
+      }, {
+        name: 'Charmander',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+      }, {
+        name: 'Charmeleon',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
+      }, {
+        name: 'Charizard',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+      }, {
+        name: 'Squirtle',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png',
+      }, {
+        name: 'Wartortle',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png',
+      }, {
+        name: 'Blastoise',
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png',
+      }, {
+        name: 'Caterpie',
         image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
       }],
     };
