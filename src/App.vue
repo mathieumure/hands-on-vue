0fd1d960ef7923737d4beea6bc5d4f888bd26f6b
Slashgear <antoine395.caron@gmail.com>
2017-12-02 21:49:46 +0100

    chore(): re-shape project with webpack-simple
    * Bulma and API in local
    * One command install
    * webpack simple, no eslint Typescript or sass
    
    Fix #11

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World !'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
