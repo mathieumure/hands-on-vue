c177345e3a8fcec15745af7be4fa91c82cd5d550
Slashgear <antoine395.caron@gmail.com>
2017-12-04 19:04:08 +0100

    feat(result): init result component

--- src/App.vue
+++ src/App.vue
@@ -10,143 +10,20 @@
       <search />
     </div>
 
-    <!-- Result set -->
-    <div class="resultset">
-      <div class="columns is-multiline">
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/1.png"/>
-              <p>
-                Bulbasaur
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/2.png"/>
-              <p>
-                Ivysaur
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/3.png"/>
-              <p>
-                Venusaur
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/4.png"/>
-              <p>
-                Charmander
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/5.png"/>
-              <p>
-                Charmeleon
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/6.png"/>
-              <p>
-                Charizard
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/7.png"/>
-              <p>
-                Squirtle
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/8.png"/>
-              <p>
-                Wartortle
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/9.png"/>
-              <p>
-                Blastoise
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Pokemon -->
-        <div class="column is-2 has-text-centered">
-          <div class="card">
-            <div class="card-content">
-              <img src="/src/assets/sprites/10.png"/>
-              <p>
-                Caterpie
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
+    <result />
 
   </div>
 </template>
 
 <script>
 import Search from './components/Search';
+import Result from './components/Result';
 
 export default {
   name: 'app',
   components: {
-    Search
+    Search,
+    Result
   }
 }
 </script>
