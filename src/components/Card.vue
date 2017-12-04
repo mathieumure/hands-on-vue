b25d5f89916d7abde4e4939c959763ced251b483
Slashgear <antoine395.caron@gmail.com>
2017-12-04 19:12:51 +0100

    feat(card): init pokemon cards

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="column is-2 has-text-centered">
+    <div class="card">
+      <div class="card-content">
+        <img :src="`/src/assets/sprites/${id}.png`"/>
+        <p>
+          {{name}}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "card",
+        props: [
+          "name",
+          "id"
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
