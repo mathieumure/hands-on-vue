4c8a595b258162ede39d76e7e1f9a14570086e66
Slashgear <antoine395.caron@gmail.com>
2017-12-04 18:58:56 +0100

    feat(search): init search component

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-danger" type="text" placeholder="Pokemon name" value="">
+    <span class="icon is-small is-left">
+          <i class="fa fa-search"></i>
+        </span>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'search',
+  }
+</script>
+
+<style>
+
+</style>
