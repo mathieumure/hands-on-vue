1965cee53499e1cec7353d6af7c4c32831037127
Antoine Caron <antoine.caron@zenika.com>
2017-12-06 13:36:30 +0100

    feat(search): filter pokemon with search criteria

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
-    <div class="control has-icons-left has-icons-right">
-      <input class="input is-danger" type="text" placeholder="Pokemon name" v-model="searchValue" @keyup="handleKeyUp"/>
-      <span class="icon is-small is-left">
-            <i class="fa fa-search"></i>
-          </span>
-    </div>
-    <h1>Your search value: {{searchValue}}</h1>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-danger" type="text" placeholder="Pokemon name" v-model="searchValue" @keyup="handleKeyUp"/>
+    <span class="icon is-small is-left">
+          <i class="fa fa-search"></i>
+        </span>
   </div>
 </template>
 
