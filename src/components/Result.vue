9175d1cf6613c553756be2bea7de7b396c111de9
Slashgear <antoine395.caron@gmail.com>
2017-12-06 22:35:53 +0100

    refactor(result): externalise pokemon in service

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -12,6 +12,7 @@
 
 <script>
     import Card from './Card';
+    import PokemonService from './pokemonService'
 
     export default {
         name: "result",
@@ -23,48 +24,7 @@
       ],
       data() {
           return {
-            pokemons: [
-              {
-                name: "Bulbasaur",
-                id: 1
-              },
-              {
-                name: "Ivysaur",
-                id: 2
-              },
-              {
-                name: "Venusaur",
-                id: 3
-              },
-              {
-                name: "Charmander",
-                id: 4
-              },
-              {
-                name: "Charmeleon",
-                id: 5
-              },
-              {
-                name: "Charizard",
-                id: 6
-              },
-              {
-                name: "Squirtle",
-                id: 7
-              },
-              {
-                name: "Wartortle",
-                id: 8
-              },
-              {
-                name: "Blastoise",
-                id: 9
-              },
-              {
-                name: "Caterpie",
-                id: 10
-              }
-            ]
+            pokemons: []
           }
       },
       methods: {
@@ -74,6 +34,9 @@
           return lowerPokemon.includes(lowerSearch);
         },
       },
+      mounted() {
+        this.pokemons = PokemonService.getPokemons();
+      }
     }
 </script>
 
