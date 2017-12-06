6d8de5293e12d8601f858ba31f55f6fa880b7143
Slashgear <antoine395.caron@gmail.com>
2017-12-06 23:37:36 +0100

    feat(pokemon): add pokemon route

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@
   import Vue from 'vue';
   import VueRouter from 'vue-router';
   import Home from './components/Home';
+  import Pokemon from './components/Pokemon';
 
   Vue.use(VueRouter);
 
   const routes = [
     { path: '/', component: Home },
+    { path: '/pokemon/:id', component: Pokemon}
   ];
 
   const router = new VueRouter({ routes });
@@ -20,6 +22,7 @@
     name: "app",
     components: {
       Home,
+      Pokemon
     },
   }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-link to="/"><h1>Home</h1></router-link>
+    <card :id="1" :name="'Bulbazor'"/>
+  </div>
+</template>
+
+<script>
+    import Card from './Card';
+
+    export default {
+        name: "pokemon",
+        components: {Card}
+    }
+</script>
+
+<style scoped>
+
+</style>
