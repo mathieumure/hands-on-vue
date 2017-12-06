b81cfdfeeb555f09c5b98048a145a1d016502828
Slashgear <antoine395.caron@gmail.com>
2017-12-06 22:35:53 +0100

    refactor(result): externalise pokemon in service

---
+++ src/components/pokemonService.js
@@ -0,0 +1,48 @@
+const PokemonService = {
+  getPokemons: () => {
+    return [
+      {
+        name: "Bulbasaur",
+        id: 1
+      },
+      {
+        name: "Ivysaur",
+        id: 2
+      },
+      {
+        name: "Venusaur",
+        id: 3
+      },
+      {
+        name: "Charmander",
+        id: 4
+      },
+      {
+        name: "Charmeleon",
+        id: 5
+      },
+      {
+        name: "Charizard",
+        id: 6
+      },
+      {
+        name: "Squirtle",
+        id: 7
+      },
+      {
+        name: "Wartortle",
+        id: 8
+      },
+      {
+        name: "Blastoise",
+        id: 9
+      },
+      {
+        name: "Caterpie",
+        id: 10
+      }
+    ];
+  }
+};
+
+export default PokemonService;
