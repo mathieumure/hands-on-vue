1e5c2cc528c9b5a2917c705693f02ef04e3d781a
Mathieu Mure <mathieu.mure@gmail.com>
2018-01-03 12:20:42 +0100

    feat(store): create store to fetch pokemons

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App'
 
 import router from './router'
+import store from './store'
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
