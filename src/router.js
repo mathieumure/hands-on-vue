769e5206a0ed6ccd56ae0635352fa4074dd930c2
Mathieu Mure <mathieu.mure@gmail.com>
2018-01-03 11:58:23 +0100

    refactor(router): use a dedicated file and set it up in main.js

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './components/Home';
+import Pokemon from './components/Pokemon';
+
+Vue.use(VueRouter);
+
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/pokemon/:id', component: Pokemon}
+];
+
+export const router = new VueRouter({ routes });
+
+export default router;
