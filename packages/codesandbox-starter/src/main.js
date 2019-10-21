8b3237a84d7a1d092ea96265d65931c35f54fe40
Slashgear <antoine395.caron@gmail.com>
2019-10-21 19:24:13 +0200

    feat(codesandbox): init codesandbox starter
    Fix #39

---
+++ packages/codesandbox-starter/src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import "./assets/bulma.css";
+import "./assets/style.css";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
